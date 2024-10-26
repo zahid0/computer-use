8537ddcc4321a9d4af49fffac412ded4f06c453e
zahid0 <zahid904@gmail.com>
2024-10-26 12:11:39 +0530

    nginx reloader changes

--- config_reloader.sh
+++ config_reloader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CONFIG_DIR="/etc/nginx/conf.d"
+CONFIG_DIR="./nginx"
 
 inotifywait -m -r -e modify,create,delete "$CONFIG_DIR" | while read path action file; do
     echo "$(date): Detected $action on $file"
