b892b61ecad4bddc87c5a1135296d1068b4ce48d
zahid0 <zahid904@gmail.com>
2024-10-26 12:23:37 +0530

    config reloader needs sudo

--- config_reloader.sh
+++ config_reloader.sh
@@ -4,5 +4,5 @@ CONFIG_DIR="./nginx"
 
 inotifywait -m -r -e modify,create,delete "$CONFIG_DIR" | while read path action file; do
     echo "$(date): Detected $action on $file"
-    nginx -t && sudo nginx -s reload
+    sudo nginx -t && sudo nginx -s reload
 done
